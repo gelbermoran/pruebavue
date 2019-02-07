dcfce63933bd8d83f20cfdbeea7773eb6b1e7a6f
Gelber Moran Silva <gmoran@parquesoftpasto.com>
2019-02-07 00:27:14 -0500

    Commit Inicial

---
+++ src/api/config.js
@@ -0,0 +1,11 @@
+/* Application name	EjemploVuejs
+API key	e06c997379c73b87ad6df44a8cc951b1
+Shared secret	459cf93878f452005a5f2aed3c9e5d51
+Registered to	gelbermoran */
+
+export default {
+    appName: 'EjemploVuejs',
+    apiKey: 'e06c997379c73b87ad6df44a8cc951b1',
+    secret: '459cf93878f452005a5f2aed3c9e5d51',
+    registeredTo: 'gelbermoran'
+}
---
+++ src/components/Artist.vue
@@ -0,0 +1,18 @@
+<template>
+  <li>
+    <h2>
+      <a :href="artist.url" target="_blank">{{ artist.name }}</a>
+      <img :src="artist.image[2]['#text']" alt>
+    </h2>
+  </li>
+</template>
+
+<script>
+export default {
+  name: "artist",
+  props: ["artist"]
+};
+</script>
+
+<style>
+</style>
